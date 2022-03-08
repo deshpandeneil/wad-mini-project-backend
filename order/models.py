a5db9b6c9550259479333fceeed58ccf9efb33c4
shivendrabhonsle <shivendrabhonsle28@gmail.com>
2022-03-08 17:14:14 +0530

    added order and product model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,25 @@
+from ast import mod
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
+
+from numpy import product
+
+from user.models import Profile
 
 # Create your models here.
+class Order(models.Model):
+    user_fk=models.ForeignKey(User,on_delete=models.CASCADE)
+    total_price=models.FloatField()
+    total_quantity=models.PositiveIntegerField()
+    eta=models.DateField()
+    
+class Item(models.Model):
+    product_fk=models.ForeignKey(Product,on_delete=models.CASCADE)
+    order_fk=models.ForeignKey(Order,on_delete=models.CASCADE)
+    price=models.FloatField()
+    quantity=models.PositiveIntegerField()
+    prescription_provided=models.BooleanField()
+
+
--- product/models.py
+++ product/models.py
@@ -1,3 +1,44 @@
+from operator import mod
+from tkinter import CASCADE
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.forms import CharField
+from numpy import product
 
 # Create your models here.
+class Uom(models.Model):
+    name=models.CharField(max_length=255)
+
+class Use(models.Model):
+    name=models.CharField(max_length=255)
+
+    
+class DiseaseCategory(models.Model):
+    name=models.CharField(max_length=255)
+
+
+class Manufacturer(models.Model):
+    name=models.CharField(max_length=255)
+    state=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    area=models.CharField(max_length=255)
+    street=models.CharField(max_length=255)
+    contact=models.CharField(max_length=255)
+
+class Product(models.Model):
+    name=models.CharField(max_length=255)
+    manufacturer_fk=models.ForeignKey(Manufacturer,on_delete=models.CASCADE)
+    disease_category_fk=models.ForeignKey(DiseaseCategory,on_delete=models.CASCADE)
+    description=models.TextField()
+    price=models.FloatField()
+    unit_size=models.PositiveIntegerField()
+    uom_fk=models.ForeignKey(Uom,on_delete=models.CASCADE)
+    power=models.PositiveIntegerField()
+    prescription_required=models.BooleanField()
+
+class ProductUses(models.Model):
+    product_fk=models.ForeignKey(Product,on_delete=models.CASCADE)
+    uses_fk=models.ForeignKey(Use,on_delete=models.CASCADE)
+    
+
+
