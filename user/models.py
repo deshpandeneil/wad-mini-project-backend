b3c0b88cee441223bfbc1a50391c1890b36c0b12
shivendrabhonsle <shivendrabhonsle28@gmail.com>
2022-03-08 14:45:20 +0530

    added profile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    phone=models.CharField(max_length=10)
+    state=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    area=models.CharField(max_length=255)
+    street=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+
