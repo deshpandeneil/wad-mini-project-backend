4b4c0860208e3d6c943636e1bac0769259c6baf1
Neil Deshpande <neildeshpande10@gmail.com>
2022-04-18 01:36:39 +0530

    added swagger

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    path('', schema_view),
     path('register/', views.UserCreate.as_view()),
     path('profiles/', views.UserList.as_view()),
     path('profiles/<int:pk>', views.UserDetail.as_view()),
