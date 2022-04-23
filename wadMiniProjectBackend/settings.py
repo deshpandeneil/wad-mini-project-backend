9be6086e2602973a746ac7fc2ddf8c6aa730232c
Neil Deshpande <neildeshpande10@gmail.com>
2022-04-23 11:52:38 +0530

    allow any host

--- wadMiniProjectBackend/settings.py
+++ wadMiniProjectBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o8#kr+$d7$5s2ds4t(i*g2%hyw=3o18_gx+xlt9auhfs2to+hz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['serenity-meds.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['serenity-meds.herokuapp.com', '127.0.0.1', '*']
 
 
 # Application definition
