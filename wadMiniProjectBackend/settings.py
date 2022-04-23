b3db098a57238a44ad2dce2b328858ab3e5ac57b
Neil Deshpande <neildeshpande10@gmail.com>
2022-04-23 12:09:14 +0530

    updated settings

--- wadMiniProjectBackend/settings.py
+++ wadMiniProjectBackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_swagger',
     'rest_framework',
+    'corsheaders',
     'order',
     'user',
     'product',
@@ -61,7 +62,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'wadMiniProjectBackend.urls'
 
